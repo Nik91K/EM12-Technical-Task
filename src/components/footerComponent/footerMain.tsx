46772c4cc9221fc9213c63e3578edb2e0689a579
Nikita <nikotakovsl@gmail.com>
2025-04-20 17:46:10 +0300

    added styles for footer

--- src/components/footerComponent/footerMain.tsx
+++ src/components/footerComponent/footerMain.tsx
@@ -1,9 +1,25 @@
 import './footerMain.css';
+import { AiFillTikTok } from "react-icons/ai";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaReddit } from "react-icons/fa";
 
 const FooterMain = () => {
     return (
         <footer className="footer-main">
-            <h2>Footer</h2>
+            <div className='footer-content'>
+                <p>@2025 Technical Task. All Reghts Reserved.</p>
+                <a href="" className='footer-link'>Terms & Conditions</a>
+            </div>
+            <div className="footer-icons">
+                <FaReddit className='footer-icon'/>  
+                <FaLinkedin className='footer-icon'/>
+                <FaTwitter className='footer-icon'/>
+                <AiFillTikTok className='footer-icon'/>
+                <FaInstagramSquare className='footer-icon'/>
+
+            </div>
         </footer>
     )
 }
