a26d38d934a831a052a178c7aef54fcb53af905a
Nikita <nikotakovsl@gmail.com>
2025-05-03 18:05:57 +0300

    categories?

--- src/types/userTypes.ts
+++ src/types/userTypes.ts
@@ -1,9 +1,9 @@
 import { Category } from "./categoryTypes";
 export type UserType = {
     id: number;
-    name: string;
+    name?: string;
     email: string;
     password: string;
-    categories: Category[];
+    categories?: Category[];
 }
   
