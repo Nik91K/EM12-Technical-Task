86d3d3fe4b3a87f0f0578ba6d6fef04dec847e8c
Nikita <nikotakovsl@gmail.com>
2025-05-03 18:05:34 +0300

    input utils

--- src/utils/getInput.ts
+++ src/utils/getInput.ts
@@ -1,7 +1,8 @@
-export function getFormInputValueByName (form: HTMLFormElement, name: string): string { 
-    const control = form.elements.namedItem(name) as HTMLInputElement; 
-    if (!control || control instanceof RadioNodeList || !("value" in control)) { 
-        throw new Error(`Form control "${name}" not found or was a RadioNodeList`); 
-    } 
-    return control.value; 
-} 
+export function getFormInputValueByName(form: HTMLFormElement, name: string): string {
+    const control = form.elements.namedItem(name) as HTMLInputElement
+    if (!control || control instanceof RadioNodeList || !("value" in control)) {
+        throw new Error(`Form control "${name}" not found or was a RadioNodeList`)
+    }
+    return control.value
+  }
+  
