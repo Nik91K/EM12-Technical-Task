766c2a1ceb128db68e513b396c5f27d1f09882c8
Nikita <nikotakovsl@gmail.com>
2025-05-05 11:54:18 +0300

    added token check

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import LoginPage from './pages/Authentication/LoginPage/loginPage.tsx'
 import TransactionsPage from './pages/TransactionsPage/TransactionsPage.tsx'
 import { Provider } from 'react-redux'
 import { store } from './api/store.ts'
+import PrivateRoute from './pages/Authentication/PrivateRoute/PrivateRoute.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
@@ -16,7 +17,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<App />} />
         <Route path='/register' element={<RegisterPage />} />
         <Route path='/login' element={<LoginPage />} />
-        <Route path='/transactions' element={<TransactionsPage />} />
+          <Route path='/transactions' element={<PrivateRoute><TransactionsPage /></PrivateRoute>} />
       </Routes>
     </BrowserRouter>
   </Provider>,
